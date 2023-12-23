e03fe27b5f5de18515a65ef752a11086c4cee93e
ranonbezerra <ranonbezerra@hotmail.com>
2023-12-23 15:53:21 -0300

    Sign in and Sign up updated.

--- fakepinterest_proj/routes.py
+++ fakepinterest_proj/routes.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
-from fakepinterest_proj import app
-from flask_login import login_required
+from flask import Flask, render_template, url_for, redirect
+from fakepinterest_proj import app, database, bcrypt
+from flask_login import login_required, login_user, logout_user
+from fakepinterest_proj.models import User, Post
 from fakepinterest_proj.forms import FormLogin, FormSignUp
 
 
@@ -11,8 +12,23 @@ def homepage():
 
 @app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
-    formsignup = FormSignUp()
-    return render_template('sign_up.html', form=formsignup)
+    form_signup = FormSignUp()
+
+    if form_signup.validate_on_submit():
+
+        password = bcrypt.generate_password_hash(form_signup.password.data).decode('utf-8')
+        user = User(username=form_signup.username.data,
+                    email=form_signup.email.data,
+                    password=password)
+        
+        database.session.add(user)
+        database.session.commit()
+        login_user(user, remember=True)
+
+        return redirect(url_for('profile', given_user=user.username))
+
+
+    return render_template('sign_up.html', form=form_signup)
 
 @app.route('/profile/<given_user>')
 @login_required
