574dce6d2c10836784bd8f057a68da1c6cd7bdd9
ranonbezerra <ranonbezerra@hotmail.com>
2023-12-27 17:19:29 -0300

    Update database

--- fakepinterest_proj/__init__.py
+++ fakepinterest_proj/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///fakepinterest_database.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config["SECRET_KEY"] = 'ad58f44870ef1d388849da006553fac3f7221d11e6defec4b2206e6df92a9d1c'
 app.config['UPLOAD_FOLDER'] = 'static/post_images'
 
